cdd05d5387f71cc5410bb1d55d9e7550308f2ef1
mayarhassan <mayarh926@gmail.com>
2025-12-02 12:19:39 +0300

    Fixing basePath to match 'my-next.js-project'

--- test-tsx/next.config.ts
+++ test-tsx/next.config.ts
@@ -1,18 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 1. تحويل الموقع لملفات HTML ثابتة
   output: "export",
-
-  // 2. تعطيل تحسين الصور (لأن GitHub Pages لا يدعم خادم الصور)
   images: {
     unoptimized: true,
   },
-
-  // 3. ضبط المسار الأساسي (هام جداً!)
-  // استبدل '/next-project' باسم مستودعك على GitHub بالضبط
-  // إذا كان رابطك: github.com/username/my-site -> اكتب '/my-site'
-  basePath: "/next-project", 
+  // ⬅️ يجب أن يكون مطابقاً تماماً لاسم المستودع الجديد
+  basePath: "/my-next.js-project", 
 };
 
 export default nextConfig;
